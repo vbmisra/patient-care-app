4df96a68fbc72773041c8e4f15cb98099134087b
Invincible <87626336+vbmisra@users.noreply.github.com>
2022-01-03 15:25:33 -0800

    updated controllers/api/patient-routes

--- controllers/api/patient-routes.js
+++ controllers/api/patient-routes.js
@@ -4,7 +4,16 @@ const { Patient } = require('../../models')
 //create a patient
 router.post('/', async (req, res) => {
     try {
+        const newPatient = await Patient.create({
+            name: req.body.name,
+            email: req.body.email,
+            symptoms: req.body.symptoms,
+            height: req.body.height,
+            weight: req.body.weight,
+            provider_id: req.body.provider_id,
+        })
 
+        res.status(200).json(newPatient)
     } catch (err) {
         res.status(400).json(err)
     }
@@ -13,7 +22,19 @@ router.post('/', async (req, res) => {
 //delete a patient
 router.delete('/:id', async (req, res) => {
     try {
+        const patientData = await Patient.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            },
+        })
 
+        if(!patientData) {
+            res.status(404).json({ message: 'No patient found with this id' })
+            return;
+        }
+
+        res.status(200).json(patientData)
     } catch (err) {
         res.status(500).json(err)
     }
