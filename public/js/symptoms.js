decc812d774042d11c94802265e5bf6bf2f93113
Invincible <87626336+vbmisra@users.noreply.github.com>
2022-01-17 21:28:30 -0800

    edited javascript

--- public/js/symptoms.js
+++ public/js/symptoms.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+//require('dotenv').config();
 
-const token = process.env.API_AUTH;
-const diagnosisURL = `https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=[16]&gender=male&year_of_birth=2012&token=${token}&format=json&language=en-gb`
-const diagnosisBtn = document.getElementById('#get-diagnosis-btn')
+//const token = process.env.API_AUTH;
+//const diagnosisURL = `https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=[16]&gender=male&year_of_birth=2012&token=${token}&format=json&language=en-gb`
+let diagnosisBtn = document.getElementById('get-diagnosis-btn')
 
 // function to retrieve diagnosis from API
 // const getDiagnosis = () => {
@@ -22,5 +22,6 @@ function getDiagnosis() {
 
 document.addEventListener(diagnosisBtn, getDiagnosis)
 
+console.log("Successful log!")
 
 
